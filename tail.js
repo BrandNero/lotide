3fccf1fcfa788063e93a79fa9aa352ddc8feade8
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-01 19:42:00 -0400

    tail

--- tail.js
+++ tail.js
@@ -13,3 +13,13 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
+
+const singleElementArray = tail(["Hello"]);
+assertEqual(singleElementArray.length, 0);
+
+const emptyArray = tail([]);
+assertEqual(emptyArray.length, 0);
+
+const words = ["Hello", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
