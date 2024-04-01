dfbfa9ba6ead5b4b2fe7ae4dc9f6887f7a0bdd14
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-01 18:35:03 -0400

    yup

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
+  
+  }
+};
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
