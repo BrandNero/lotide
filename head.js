99fe0932a09cfad2db620621515bf391ee1f3e85
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-01 19:02:03 -0400

    yup

--- head.js
+++ head.js
@@ -6,10 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 const head = function(array) {
-  if (array.length > 0) {
-    return array[0];
-  }
-  return undefined;
+  return array[0];
 };
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
