8f0f4dce657dfa2eba8ad1aeb7d03b563a4dbbd2
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-01 18:56:50 -0400

    yup

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(array) {
+  if (array.length > 0) {
+    return array[0];
+  }
+  return undefined;
+};
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
