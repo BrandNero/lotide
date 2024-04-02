e991c336b17391336d4c5832e9627839efab9927
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-02 19:33:39 -0400

    arr.length add false

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
+  
+  }
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(false);
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(false);
+      } else {
+        console.log(true);
+      }
+    }
+  }
+};
+//eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3, 4], [3, 2, 1]); // => false
+
+//eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+//eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
