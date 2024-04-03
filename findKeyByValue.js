968958e6b57281420a77d98b251f9e6b345b60f8
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-03 19:06:23 -0400

    add find

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
+  
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let keyOfObject = Object.keys(object);
+  for (const shows of keyOfObject) {
+    if (object[shows] === value) {
+      return shows;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
