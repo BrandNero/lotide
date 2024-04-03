40a08a9e0366c9964f90474217e322334dc24daf
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-02 20:07:01 -0400

    fixed ai feedback

--- eqArrays.js
+++ eqArrays.js
@@ -8,19 +8,18 @@ const assertEqual = function(actual, expected) {
 };
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    console.log(false);
+    return false;
   } else {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
-        console.log(false);
-      } else {
-        console.log(true);
+        return false;
       }
     }
+    return true;
   }
 };
-//eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3, 4], [3, 2, 1]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+assertEqual(eqArrays(([1, 2, 3, 4], [3, 2, 1])); // => false
 
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-//eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
