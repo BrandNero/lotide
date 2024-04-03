5785c1e883d72301dbcd9709a7799cc1f8569572
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-03 16:59:24 -0400

    add countLetters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
+  
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+countLetters("lighthouse");
