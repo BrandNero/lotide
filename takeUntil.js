f0b159025cdc33bf11ebb2c58c23de43b4eff100
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-04 19:22:27 -0400

    add takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -32,11 +32,11 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-const expectedResults1 = [ 1, 2, 5, 7, 2 ]
+const expectedResults1 = [ 1, 2, 5, 7, 2 ];
 assertArraysEqual(results1, expectedResults1);
 
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-const expectedResults2 = [ "I've", 'been', 'to', 'Hollywood' ]
+const expectedResults2 = [ "I've", 'been', 'to', 'Hollywood' ];
 assertArraysEqual(results2, expectedResults2);
