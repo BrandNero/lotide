742648b1cf96745147ceef497a68d4bbc763ef6e
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-04 19:26:06 -0400

    added ai comments'

--- takeUntil.js
+++ takeUntil.js
@@ -23,7 +23,7 @@ const takeUntil = function(array, callback) {
   let result = [];
   for (const item of array) {
     if (callback(item)) {
-      break;
+      break; //stop the loop when a callback is met
     }
     result.push(item);
   }
@@ -31,12 +31,12 @@ const takeUntil = function(array, callback) {
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
+const results1 = takeUntil(data1, x => x < 0); // takeUntil(array, callback)
 const expectedResults1 = [ 1, 2, 5, 7, 2 ];
 assertArraysEqual(results1, expectedResults1);
 
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
+const results2 = takeUntil(data2, x => x === ','); // takeUntil(array, callback)
 const expectedResults2 = [ "I've", 'been', 'to', 'Hollywood' ];
 assertArraysEqual(results2, expectedResults2);
