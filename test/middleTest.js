c989f58e848490b6bc6b6de9720e5ed43902a990
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-08 13:50:56 -0400

    mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,18 @@
+const assert = require("chai").assert;
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#assertEqual", () => {
+  it("return the middle from an array with 1 index", () => {
+    assert.strictEqual(assertArraysEqual(middle([1]), []));
+  });
+  it("returns the middle from an array with 2 index", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2]), []));
+  });
+  it("return the middle from an array with 4 index.", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+  });
+  it("returns the middle of an array with 5 index", () => {
+    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,28 @@
+const assert = require("chai").assert;
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
 const singleElementArray = tail(["Hello"]);
-assertEqual(singleElementArray.length, 0);
-
 const emptyArray = tail([]);
-assertEqual(emptyArray.length, 0);
-
 const words = ["Hello", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#assertEqual", () => {
+  it("return true from 2", () => {
+    assert.strictEqual(assertEqual(result.length, 2));
+  });
+  it("returns return the first index from an array", () => {
+    assert.strictEqual(assertEqual(result[0], "Lighthouse"));
+  });
+  it("return the second index from an array", () => {
+    assert.strictEqual(assertEqual(result[1], "Labs"));
+  });
+  it("returns the length from an array with 1 index", () => {
+    assert.strictEqual(assertEqual(singleElementArray.length, 0));
+  });
+  it("returns the length from an array with no index", () => {
+    assert.strictEqual(assertEqual(emptyArray.length, 0));
+  });
+  it("returns the length from an array", () => {
+    assert.strictEqual(assertEqual(words.length, 3));
+  });
+});
