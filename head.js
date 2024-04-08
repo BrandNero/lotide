1ad7528cd34be3e69ca39efb1192ee3f2a3bec84
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-08 10:54:30 -0400

    refactor

--- head.js
+++ head.js
@@ -1,12 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🟥🟥🟥Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
+
 const head = function(array) {
   return array[0];
 };
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+module.exports = head;
