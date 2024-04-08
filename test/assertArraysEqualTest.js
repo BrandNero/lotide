900aa8e4a2e6eb24a73f4252189d2a0c43cdfaf8
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-08 13:53:22 -0400

    mocha

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -7,10 +7,16 @@ const assertArraysEqual = require("../assertArraysEqual");
 // assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
 // assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
 describe("#assertArraysEqual", () => {
-  it("return true from [1, 2, 3]", () => {
+  it("return true from arrays when  same", () => {
     assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
   });
-  it("returns true from [5]", () => {
-    assert.strictEqual(assertArraysEqual([5], [5]));
+  it("returns true from an array with string numbers", () => {
+    assert.strictEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
+  });
+  it("returns false from an array with numbers in wrong postion", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+  });
+  it("returns true from an array with string numbers", () => {
+    assert.strictEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
   });
 });
