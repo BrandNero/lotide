2400dba522670052989ae0949982f440c19ec25d
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-08 11:45:16 -0400

    refactor

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🟥🟥🟥 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -26,9 +7,4 @@ const middle = function(array) {
   }
   return [array[Math.floor(array.length / 2)]];
 };
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
